e39550023ed48dee7fcb716518c162aad1335f36
Derek DeCoste <dsquaredslim@gmail.com>
2018-05-05 16:56:11 -0700

    bets working on create page

--- frontend/client/src/components/CreateBet/index.jsx
+++ frontend/client/src/components/CreateBet/index.jsx
@@ -8,9 +8,9 @@ class CreateBet extends React.Component { // note we do not export the actual Re
     super(props)
     this.state = {
       end: '', // this should be calendar
-      club: '', // should default to the club they are on
-      wager: '',
-      description: '',
+      club: '', // should be a drop down and default to currently selected club
+      wager: '', // text field input
+      description: '', // text field input
       odds: '1:1', // be able to change ultimately
     }
     this.handleChange = this.handleChange.bind(this);
@@ -18,7 +18,7 @@ class CreateBet extends React.Component { // note we do not export the actual Re
   }
 
   handleChange(e) {
-    this.setState({ value: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
@@ -29,17 +29,10 @@ class CreateBet extends React.Component { // note we do not export the actual Re
 
   render() {
     return (
-      <div>Hello from React router create bet
-            your current store is as follows!!!
-            {/* {this.props.store.getState()} */}
-
-            and this.props.createNumber is '{JSON.stringify(this.props.createNumber)}'
-            and this.props.searchNumber is '{JSON.stringify(this.props.searchNumber)}'
-            <button onClick={()=> console.log(this.props.createNumber)}/>
       <div>
+        <input type="text" name="club" value={this.state.value} onChange={this.handleChange} />
         <input type="text" value={this.state.value} onChange={this.handleChange} />
         <input type="submit" value="Submit" onClick={this.handleSubmit}/>
-      </div>
       </div>
     );
   }
