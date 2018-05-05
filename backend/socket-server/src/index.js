bca4ac24dbba6f2dede4e22926aaa53eb8b3b5fc
Warren <warren.p.kwong@gmail.com>
2018-05-05 11:20:49 -0700

    Learning sockets

--- backend/socket-server/src/index.js
+++ backend/socket-server/src/index.js
@@ -1,1 +1,21 @@
-// empty file
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on PORT:3000');
+});
