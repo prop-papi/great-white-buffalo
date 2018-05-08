a4cce711481c9f77a00dc63e24af7cc3fe44afef
Nikko Sanchez <neeks08@gmail.com>
2018-05-08 14:22:47 -0700

    Pre rebase

---
+++ frontend/client/src/components/LoungeUsers/UserList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
