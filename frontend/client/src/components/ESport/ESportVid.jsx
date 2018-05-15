a160744a7ab1d8e14b9ce1b4dce6b8363d3bb447
Nikko Sanchez <neeks08@gmail.com>
2018-05-15 15:02:45 -0700

    Removed unnecessary console logs

--- frontend/client/src/components/ESport/ESportVid.jsx
+++ frontend/client/src/components/ESport/ESportVid.jsx
@@ -20,28 +20,20 @@ class ESportVid extends Component {
   }
 
   componentDidMount() {
-    console.log("in did mount");
     const name = this.props.local.club.name;
     this.setState({ game: this.state.gamesList[name] });
     this.getStream(TWITCH_CLIENT_ID);
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    console.log("nextProps here: ", nextProps);
-    console.log("prevState here: ", prevState);
     const name = nextProps.local.club.name;
-    if (prevState.gamesList[name] !== prevState.game) {
-      return {
-        game: prevState.gamesList[name]
-      };
-    }
-    return null;
+    return prevState.gamesList[name] !== prevState.game
+      ? { game: prevState.gamesList[name] }
+      : null;
   }
 
   async componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps);
     if (prevState.game !== this.state.game) {
-      console.log("in if statement for component did update");
       await this.getStream(TWITCH_CLIENT_ID);
     }
   }
@@ -60,8 +52,6 @@ class ESportVid extends Component {
       )
       .then(response => {
         this.setState({ streamer: response.data.streams[0].channel.name });
-
-        //return response.data.streams[0].channel.name;
       })
       .catch(err => {
         console.log(err);
@@ -69,12 +59,9 @@ class ESportVid extends Component {
   }
 
   render() {
-    console.log("before if state render", this.state.streamer);
     if (this.state.streamer) {
-      console.log("in if state streamer: ", this.state.streamer);
       return (
         <div align="center">
-          {console.log("fdjsaklf;dsakl;", this.state.streamer)}
           <ResponsiveEmbed a4by3>
             <iframe
               src={`http://player.twitch.tv/?channel=${this.state.streamer}`}
