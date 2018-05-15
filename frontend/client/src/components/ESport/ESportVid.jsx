81c6a5c2b98337f3fca27e203798c7e251645bf3
Nikko Sanchez <neeks08@gmail.com>
2018-05-15 14:54:32 -0700

    Pulling most viewed streamer and rendering there twitch stream now correctly working

--- frontend/client/src/components/ESport/ESportVid.jsx
+++ frontend/client/src/components/ESport/ESportVid.jsx
@@ -1,29 +1,67 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { ResponsiveEmbed, Embed } from "react-bootstrap";
+import { ResponsiveEmbed } from "react-bootstrap";
 import { TWITCH_CLIENT_ID } from "../../../../../config.js";
+import { connect } from "react-redux";
 
 class ESportVid extends Component {
   constructor() {
     super();
     this.state = {
-      streamer: ""
+      streamer: "",
+      game: "",
+      gamesList: {
+        Fortnite: "fortnite",
+        Overwatch: "overwatch",
+        "Rocket League": "Rocket League",
+        PUBG: "playerunknown's battlegrounds"
+      }
     };
   }
 
   componentDidMount() {
+    console.log("in did mount");
+    const name = this.props.local.club.name;
+    this.setState({ game: this.state.gamesList[name] });
     this.getStream(TWITCH_CLIENT_ID);
   }
 
-  getStream(clientID) {
-    axios
-      .get("https://api.twitch.tv/kraken/streams/?game=fortnite&limit=1", {
-        headers: {
-          "Client-ID": clientID
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("nextProps here: ", nextProps);
+    console.log("prevState here: ", prevState);
+    const name = nextProps.local.club.name;
+    if (prevState.gamesList[name] !== prevState.game) {
+      return {
+        game: prevState.gamesList[name]
+      };
+    }
+    return null;
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate", prevProps);
+    if (prevState.game !== this.state.game) {
+      console.log("in if statement for component did update");
+      await this.getStream(TWITCH_CLIENT_ID);
+    }
+  }
+
+  async getStream(clientID) {
+    await axios
+      .get(
+        `https://api.twitch.tv/kraken/streams/?game=${
+          this.state.gamesList[this.props.local.club.name]
+        }&limit=1`,
+        {
+          headers: {
+            "Client-ID": clientID
+          }
         }
-      })
+      )
       .then(response => {
         this.setState({ streamer: response.data.streams[0].channel.name });
+
+        //return response.data.streams[0].channel.name;
       })
       .catch(err => {
         console.log(err);
@@ -31,11 +69,14 @@ class ESportVid extends Component {
   }
 
   render() {
+    console.log("before if state render", this.state.streamer);
     if (this.state.streamer) {
+      console.log("in if state streamer: ", this.state.streamer);
       return (
-        <div align="center" id="twitch">
+        <div align="center">
+          {console.log("fdjsaklf;dsakl;", this.state.streamer)}
           <ResponsiveEmbed a4by3>
-            <embed
+            <iframe
               src={`http://player.twitch.tv/?channel=${this.state.streamer}`}
             />
           </ResponsiveEmbed>
@@ -46,4 +87,10 @@ class ESportVid extends Component {
   }
 }
 
-export default ESportVid;
+const mapStateToProps = state => {
+  return {
+    local: state.local.localData
+  };
+};
+
+export default connect(mapStateToProps)(ESportVid);
