3d2355944ab4d3abd7b6afb3a0aaee903e725a75
Warren <warren.p.kwong@gmail.com>
2018-05-16 08:27:14 -0700

    Leaderboards complete -- can sort by fields: wins, losses, totalBets, winPercentage, Tokens

---
+++ backend/services/leaderboard-fetcher/src/utils/index.js
@@ -0,0 +1,12 @@
+const {
+  getTop100UsersBy
+} = require("../../../../rest-server/src/db/models/users/");
+const redis = require("redis");
+const redisClient = require("../../../../redis-server/src/").client;
+
+const getUsersBy = async columnName => {
+  const data = await getTop100UsersBy(columnName);
+  await redisClient.set(columnName, JSON.stringify(data));
+};
+
+module.exports.getUsersBy = getUsersBy;
